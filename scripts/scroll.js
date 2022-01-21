9a6125142bef3301df880658e7dbecae7761ab64
Gala Slesareva <67021657+slesareva-gala@users.noreply.github.com>
2022-01-21 20:02:28 +0300

    Add files via upload

---
+++ scripts/scroll.js
@@ -0,0 +1,37 @@
+// доступ к первому ребенку с тегом a первого родителя с классом .header-menu__item
+// const link = document.querySelector('.header-menu__item a')
+// доступ к списку узлов (nodelist) первых детей с тегом a родителей с классом .header-menu__item
+// - ссылки пунктов меню
+const links = document.querySelectorAll('.header-menu__item a, .card-details__link-characteristics')
+console.log(links)
+
+// подключение полифила плавного скролла
+// https://www.npmjs.com/package/seamless-scroll-polyfill
+seamless.polyfill();
+
+links.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        // отключить действия по умолчанию по клику мыши (переход по ссылке)
+        event.preventDefault()
+
+        // id элемента, на который нужно перейти по нажатию пункта меню
+        const id = element.getAttribute('href').slice(1)
+        // ссылка на элемент с заданным id
+        const section = document.getElementById(id)
+        if (section) {  // для имеющих секцию
+            // устанавливаем плавный переход
+            // section.scrollIntoView({   // на Safari не работает
+            // ... а это должно работать везде (кроссбраузерный)
+            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                behavior: 'smooth',
+                block: 'start'
+            })
+        } else {
+            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+        }
+    })
+})
