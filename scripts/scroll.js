81097a62180807d00eb4b67a4a6f72eeccb9927d
Gala Slesareva <67021657+slesareva-gala@users.noreply.github.com>
2022-01-22 18:43:21 +0300

    День 2

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,36 +1,44 @@
-// доступ к первому ребенку с тегом a первого родителя с классом .header-menu__item
-// const link = document.querySelector('.header-menu__item a')
-// доступ к списку узлов (nodelist) первых детей с тегом a родителей с классом .header-menu__item
-// - ссылки пунктов меню
-const links = document.querySelectorAll('.header-menu__item a, .card-details__link-characteristics')
+const scrollFunc = () => { // инкапсулируем...
+    // доступ к первому ребенку с тегом a первого родителя с классом .header-menu__item
+    // const link = document.querySelector('.header-menu__item a')
+    // доступ к списку узлов (nodelist) первых детей с тегом a родителей с классом .header-menu__item
+    // - ссылки пунктов меню
+    const links = document.querySelectorAll('.header-menu__item a, .card-details__link-characteristics')
+    /* вариант №2 (объединение/добавление nodelist)
+      const linkCharacteristics = document.querySelector('.card-details__link-characteristics')
+      const newArray = [...links, linkCharacteristics]
+      и далее, вместо links обрабатываем newArray
+     */
 
-// подключение полифила плавного скролла
-// https://www.npmjs.com/package/seamless-scroll-polyfill
-seamless.polyfill();
+    // подключение полифила плавного скролла
+    // https://www.npmjs.com/package/seamless-scroll-polyfill
+    seamless.polyfill();
 
-links.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        // отключить действия по умолчанию по клику мыши (переход по ссылке)
-        event.preventDefault()
+    links.forEach((element) => {
+        element.addEventListener('click', (event) => {
+            // отключить действия по умолчанию по клику мыши (переход по ссылке)
+            event.preventDefault()
 
-        // id элемента, на который нужно перейти по нажатию пункта меню
-        const id = element.getAttribute('href').slice(1)
-        // ссылка на элемент с заданным id
-        const section = document.getElementById(id)
-        if (section) {  // для имеющих секцию
-            // устанавливаем плавный переход
-            // section.scrollIntoView({   // на Safari не работает
-            // ... а это должно работать везде (кроссбраузерный)
-            seamless.elementScrollIntoView(section, {
-                behavior: 'smooth',
-                block: 'start'
-            })
-        } else {
-            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-                behavior: "smooth",
-                block: "center",
-                inline: "center",
-            });
-        }
+            // id элемента, на который нужно перейти по нажатию пункта меню
+            const id = element.getAttribute('href').slice(1)
+            // ссылка на элемент с заданным id
+            const section = document.getElementById(id)
+            if (section) {  // для имеющих секцию
+                // устанавливаем плавный переход
+                // section.scrollIntoView({   // на Safari не работает
+                // ... а это должно работать везде (кроссбраузерный)
+                seamless.elementScrollIntoView(section, {
+                    behavior: 'smooth',
+                    block: 'start'
+                })
+            } else {
+                seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "center",
+                });
+            }
+        })
     })
-})
+}
+scrollFunc()
---
+++ scripts/tabs.js
@@ -0,0 +1,72 @@
+const tabsFunc = () => {  // инкапсулируем...
+    // ссылки табуляторов
+    const tabs = document.querySelectorAll('.card-detail__change')
+    // ссылка на заголовок товара (смартфон)
+    const tabsTitle = document.querySelector('.card-details__title')
+    // ссылка на цену товара (смартфон)
+    const tabsPrice = document.querySelector('.card-details__price')
+    // ссылка на фото товара (смартфон)
+    const tabsImage = document.querySelector('.card__image_item')
+    //  ccылка на title вкладки браузера 
+    const titlePage = document.querySelector('title')
+
+    // параметры моделей смартфона
+    const tabsOption = [
+        {
+            name: "Graphite",
+            namePage: "Графитовый",
+            memory: '128',
+            price: 60000,
+            image: 'img/iPhone-graphite.webp'
+        },
+        {
+            name: "Silver",
+            namePage: "Серебристый",
+            memory: '256',
+            price: 65000,
+            image: 'img/iPhone-silver.webp'
+        },
+        {
+            name: "Sierra Blue",
+            namePage: "Небесно-голубой",
+            memory: '512',
+            price: 70000,
+            image: 'img/iPhone-sierra_blue.webp'
+        }
+    ]
+
+    // смена контента
+    const changeContent = (index) => {
+        // меняем описание товара
+        const cur = tabsOption[index]
+        tabsTitle.textContent = `Смартфон Apple iPhone 13 Pro ${cur.memory}GB ${cur.name}`
+        tabsPrice.textContent = `${cur.price}₽`
+        // меняем заголовок страницы
+        titlePage.textContent = cur.namePage
+        // меняем фото товара
+        tabsImage.setAttribute('src', cur.image)
+    }
+    // переключатель табов
+    const changeActiveTabs = (indexClickedTab) => {
+        tabs.forEach((tab, index) => {
+            // снимаем подсветку со всех
+            tab.classList.remove('active')
+            // устанавливаем подсветку на кликнутом
+            if (index == indexClickedTab) {
+                tab.classList.add('active')
+            }
+        })
+        // меняем контент в соответствии с выбором
+        changeContent(indexClickedTab)
+    }
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            changeActiveTabs(index)
+        })
+    })
+
+    // установка первоначального выбора модели (№ 0 при загрузке страницы)
+    changeContent(0)
+}
+tabsFunc()
